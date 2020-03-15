dbe89612959182de06cde4554ef6334b40c9af96
Rui MADEIRA <rui.madeira.e@thalesdigital.io>
2020-03-15 20:25:14 +0000

    Further completed explanation in 3_15 with some of my thoughts and fixed typos on same exercise.

--- src/exercise_3_15.scala
+++ src/exercise_3_15.scala
@@ -18,12 +18,18 @@ class exercise_3_15 {
   def concat[A](l: List[List[A]]): List[A] = reverse(foldLeft(reverse(l), Nil: List[A])((a, b) => foldLeft(b, a)((a, b) => Cons(b, a))))
 
   // Book solution uses append in the concat, I think is equivalent to what I wrote.
-  // I don't understand the right-associativity explain part of the solution:
+  // I don't understand the right-associativity explanation part of the solution:
   // 'and this first argument never grows because of the right-associativity of `foldRight`'.
-  // Interesting point of the solution as it says is the way the reference append.
+  // Interesting point of the solution as it says is the way they reference append.
   def append[A](l: List[A], r: List[A]): List[A] =
     foldRight(l, r)(Cons(_,_))
 
   def concat[A](l: List[List[A]]): List[A] =
     foldRight(l, Nil:List[A])(append)
+
+  // After reading https://en.wikipedia.org/wiki/Operator_associativity I think an example popped in my mind for the
+  // associativity part, basically if foldRight was not right-associative each time a new list is processed and joined
+  // with the previous we would have to iterate that bigger list in order to add the new processed one to it's end,
+  // depending on the scenario this would not be linear on total length of lists since some lists could be iterated
+  // several times
 }
